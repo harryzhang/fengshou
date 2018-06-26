25859e77a4b76e6f581216bcfe224924d3b1551e
Your Name <zhangyunhmf@hyn.com>
2018-06-26 19:31:17 +0800

    handler

--- kder-web/src/main/java/com/kder/business/common/exception/KderGlobalExceptionHandler.java
+++ kder-web/src/main/java/com/kder/business/common/exception/KderGlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.kder.business.common.exception;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,9 +10,12 @@ import com.kder.business.common.result.Result;
 @ControllerAdvice
 public class KderGlobalExceptionHandler {
 
+	private Logger logger = Logger.getLogger(KderGlobalExceptionHandler.class);
+	
     @ExceptionHandler(BusinessException.class)
 	@ResponseBody
     public Result<?> handleCustomerException(BusinessException ex) {
+    	logger.error(ex);
     	System.out.println("global BusinessException");
         return Result.failureResult(ex.getMessage());
     }
@@ -20,7 +24,8 @@ public class KderGlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public Result<?> handleAllException(Exception ex) {
-    	System.out.println("global exception");
+    	logger.error(ex);
+    	System.out.println("global exception:");
     	return Result.failureResult(ex.getMessage());
     }
 }
